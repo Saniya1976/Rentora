08c70aa4895c73488763fe72ef81152a12e16888
Saniya1976 <saniyakumari1976@gmail.com>
2026-02-14 16:47:23 +0530

    name change of dashboard

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,17 +1,17 @@
 import Navbar from "@/components/Navbar"
-import HeroSection from "./(dashboard)/landing/HeroSection";
-import FeaturesSection from "./(dashboard)/landing/FeaturesSection";
-import DiscoverSection from "./(dashboard)/landing/DiscoverSection";
-import CallToActionSection from "./(dashboard)/landing/CallToActionSection";
+import HeroSection from "./(nondashboard)/landing/HeroSection";
+import FeaturesSection from "./(nondashboard)/landing/FeaturesSection";
+import DiscoverSection from "./(nondashboard)/landing/DiscoverSection";
+import CallToActionSection from "./(nondashboard)/landing/CallToActionSection";
 
 export default function Home() {
   return (
     <div className="bg-white min-h-screen">
-     <Navbar />
-     <HeroSection/>
-     <FeaturesSection/>
-     <DiscoverSection/>
-     <CallToActionSection/>
+      <Navbar />
+      <HeroSection />
+      <FeaturesSection />
+      <DiscoverSection />
+      <CallToActionSection />
     </div>
   );
 }
