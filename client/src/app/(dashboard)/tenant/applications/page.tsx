98db92773e292511ac01373284f8566f08b2b559
Saniya1976 <saniyakumari1976@gmail.com>
2026-03-28 00:36:51 +0530

    application creation

--- client/src/app/(dashboard)/tenant/applications/page.tsx
+++ client/src/app/(dashboard)/tenant/applications/page.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const ApplicationsPage = () => {
     return (
         <div className="flex flex-col gap-4">
-            <h1 className="text-4xl font-black bg-gradient-to-r from-[#1acec8] to-[#15b8b3] bg-clip-text text-transparent uppercase tracking-tight">
+            <h1 className="text-4xl font-black bg-linear-to-r from-[#1acec8] to-[#15b8b3] bg-clip-text text-transparent uppercase tracking-tight">
                 Applications
             </h1>
             <p className="text-gray-500 dark:text-zinc-400 text-sm italic">
